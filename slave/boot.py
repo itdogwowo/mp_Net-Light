78d4dafe34f60df49c096a1d2170804ba2f50df7
TVB-OA\bl91920 <itdogwowo@gmail.com>
2026-02-12 18:03:32 +0800

    no message

--- slave/boot.py
+++ slave/boot.py
@@ -9,14 +9,49 @@ def init_lan(sysBus):
     """初始化 LAN 硬件，但不阻塞等待連接"""
     # 這裡保持你的硬體配置
     config = sysBus.shared['ETH_Network']
-    lan = network.LAN(mdc=config['list'][0]['GPIO']['mdc'], mdio=config['list'][0]['GPIO']['mdio'],
-                      ref_clk=config['list'][0]['GPIO']['ref_clk'], phy_addr=config['list'][0]['phy_type'],
-                      phy_type=network.PHY_IP101 )
-    lan.active(True)
-    sysBus.register_service("lan", lan)
+    if config['enable']:
+        for i in config['list']:
+            lan = network.LAN(mdc=i['GPIO']['mdc'], mdio=i['GPIO']['mdio'],
+                          ref_clk=i['GPIO']['ref_clk'], phy_addr=i['phy_addr'],
+                          phy_type=i['phy_type'] )
+            
+        lan.active(True)
+        sysBus.register_service("lan", lan)
+    return
+
+def init_bus(sysBus):
+    import machine
+    # 這裡保持你的硬體配置
+    SPI_config = sysBus.shared['SPI']
+    spi_list = []
+    if SPI_config['enable']:
+        for i in SPI_config['list']:
+            spi = machine.SPI(i['id'],
+                baudrate=i['baudrate'],
+                polarity=i['polarity'],
+                phase=i['phase'],
+                sck=machine.Pin(i['GPIO']['sck']) if i['GPIO']['sck'] else None ,
+                mosi=machine.Pin(i['GPIO']['mosi']) if i['GPIO']['mosi'] else None ,
+                miso=machine.Pin(i['GPIO']['miso']) if i['GPIO']['miso'] else None
+            )
+            spi_list.append(spi)            
+            
+        print(SPI_config)
+        sysBus.register_service("spi_list", spi_list)
+    return
+
+def init_led(sysBus):
+    # 這裡保持你的硬體配置
+
     return
 
 
-init_lan(bus)
 
+init_lan(bus)
+init_bus(bus)
+init_led(bus)
+
+sss = bus.get_service("spi_list")
 print(bus.get_service("lan"))
+print(bus.get_service("spi_list"))
+# bus.shared['ETH_Network']
